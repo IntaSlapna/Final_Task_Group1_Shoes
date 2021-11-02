29198418d60ef61d936c5c79eeb2682f6a98995d
Inta.Slapna <inta.slapnja@inbox.lv>
2021-11-02 22:44:34 +0200

    Changed If to Switch in InputMethods

--- src/com/company/InputMethods.java
+++ src/com/company/InputMethods.java
@@ -1,14 +1,14 @@
 package com.company;
-import java.sql.*;
 import java.util.Scanner;
 
-public class Main {
+public class InputMethods {
 
     public static void main(String[] args) {
     inputSize();
     inputColour();
     inputBrand();
     }
+
     public static void inputSize () {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! We are glad that you are using our shoes store.\n" +
@@ -31,28 +31,35 @@ public class Main {
     }
     public static void inputColour () {
         Scanner input = new Scanner(System.in);
-        System.out.println("\n What colour do you prefer?\n" +
-                "Write R for red, W for white, B for black, BR for brown");
+        System.out.println("\nWhat colour do you prefer?\n" +
+                "Write R for red, W for white, B for black, BR for brown:");
         String colour = input.next();
-        if (colour == "R" || colour == "W" || colour == "B" || colour == "BR")
-            System.out.println("Your choise is add to the filter!");
-        else
-            System.out.println("At the moment there is no such colour! \n" +
-                    "Choose from existing ones or came back on December 1st, we are going to have new collection!");
-
+        switch (colour) {
+            case "R": case "W": case "B": case "BR":
+                System.out.println("Your choise is added to the filter!");
+                break;
+            default:
+                System.out.println("At the moment there is no such colour! \n" +
+                        "Choose from existing ones or came back on December 1st, we are going to have new collection!");
+                break;
+        }
     //jāpievieno opcija, kāmēr netiek uzstādīts filtrs, tikmēr nevar pāriet pie nākamā jautājuma
     }
 
     public static void inputBrand () {
         Scanner input = new Scanner(System.in);
-        System.out.println("\n What brand do you prefer?\n" +
-                "Write RL for Ralph Lauren, TM for Tommy Hilfiger, CK for Calvin Klein, KL for Karl Lagerfeld");
+        System.out.println("\nWhat brand do you prefer?\n" +
+                "Write RL for Ralph Lauren, TM for Tommy Hilfiger, CK for Calvin Klein, KL for Karl Lagerfeld:");
         String brand = input.next();
-        if (brand == "RL" || brand == "TM" || brand == "CK" || brand == "KL")
-            System.out.println("Your choise is add to the filter!");
-        else
-            System.out.println("At the moment there is no such brand! \n" +
-                "We are working on expanding our assortment!");
+        switch (brand) {
+            case "RL": case "TM": case "CK": case "KL":
+                System.out.println("Your choise is added to the filter!");
+                break;
+            default:
+                    System.out.println("At the moment there is no such brand! \n" +
+                            "We are working on expanding our assortment!");
+                    break;
+        }
 
         //jāpievieno opcija, kāmēr netiek uzstādīts filtrs, tikmēr nevar pāriet pie nākamā jautājuma
     }
